6e36f323df65c2f1c282d82b23dd85cbd6e518bb
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-04-19 00:45:27 -0700

    able to verify password

--- server/index.js
+++ server/index.js
@@ -18,16 +18,32 @@ app.post('/users', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.query.password, salt);
-    
+
     const user = { 
       username: req.query.username,
       password: hashedPassword,
-     };
+    };
     users.push(user);
     res.status(201).send(users);
   } catch {
     res.status(500).send();
   }
 });
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.username === req.query.username)
+  if (user === null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if (await bcrypt.compare(req.query.password, user.password)) {
+      res.send('Your logged in!')
+    } else {
+      res.send('Incorrect password')
+    }
+  } catch {
+      res.status(500).send()
+    }
+})
 
 app.listen(port, () => console.log(`Mounting on port: ${port}`));
