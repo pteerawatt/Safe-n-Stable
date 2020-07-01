037f991971da66b71aec8b587739b81d9ce15e08
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-07-01 16:10:59 -0700

    ? button is there

--- client/src/component/AddMount.jsx
+++ client/src/component/AddMount.jsx
@@ -12,6 +12,7 @@ const AddMount = ({ AddMountByName }) => {
       <div className="form-group">
         <input className="form-control" placeholder="Add mount to Stable" type="text" id="mountName" name="mountName" value={mountName} required onChange={(event) => { setName(event.target.value); }}></input>
         <button className="btn btn-dark add-mount-btn">Add</button>
+        <button type="button" class="btn btn-info" style={{ marginLeft: '8px' }}>?</button>
       </div>
     </form>
   );
